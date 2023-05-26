60057d3d0ed26ac88d61208448d28ca2c1f2a383
James Kyoung Ho Kim <james19190@gmail.com>
2023-05-26 17:49:10 +0900

    Create 88. Merge Sorted Array.java

---
+++ 88. Merge Sorted Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int count = 0;
+        for (int i = m; i < nums1.length; i++){
+            nums1[i] = nums2[count++];
+        }
+
+        Arrays.sort(nums1);
+
+
+    }
+
+    
+}

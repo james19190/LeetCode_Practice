76d69673286f225a6281034ac64209f88135efaf
James Kyoung Ho Kim <james19190@gmail.com>
2023-05-26 17:59:39 +0900

    Create 283. Move Zeroes.java

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int i = 0;
+
+        for(int j  = 0; j < nums.length; j++) {
+
+            if(nums[j] != 0) {
+                nums[i] = nums[j];
+                if(j > i) {
+                    nums[j] = 0;   
+                }
+                i++;
+            } 
+        }
+
+    }
+}

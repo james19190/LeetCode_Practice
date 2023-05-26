a9a4490650916db4908ca0e068db30b77bd88e57
James Kyoung Ho Kim <james19190@gmail.com>
2023-05-26 17:48:48 +0900

    Create 70. Climbing Stairs.java

---
+++ 70. Climbing Stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int[] vals = new int[n];
+        if(n ==1){return 1;}
+
+        vals[0] = 1;
+        vals[1] = 2;
+
+        for (int i = 2; i < n ; i++){
+            vals[i] = vals[i-1] + vals[i-2]; 
+        }
+
+        return vals[n-1];
+    }
+}
